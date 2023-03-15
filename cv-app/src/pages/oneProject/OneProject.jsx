782d778f4be3da11833b0afdc2769c34b6ba7013
Victor Buga <bugavictor86@gmail.com>
2023-03-15 07:47:33 +0100

    one project data bat

--- cv-app/src/pages/oneProject/OneProject.jsx
+++ cv-app/src/pages/oneProject/OneProject.jsx
@@ -8,19 +8,20 @@ import { useParams } from "react-router-dom";
 export default function OneProject() {
   const { t } = useSelector((state) => state.langReducer);
   const { id } = useParams();
-  const data = t.cardArch.find((product) => product.id === id);
+  const hrefArch = window.location.href.includes(t.archNav);
+  const hrefBat = window.location.href.includes(t.batNav);
+
+  let data;
+
+  hrefArch && (data = t.cardArch.find((product) => product.id === id));
+  hrefBat && (data = t.cardBat.find((product) => product.id === id));
 
   console.log("data", data);
   const { pictures, title, info } = data;
   return (
     <main className="main_oneProjectBtm main-scroll">
-      {/* <h1 className="h1_one_projectBtm">{title}</h1> */}
       <div className="DD_wrapper">
-        <Dropdown
-          title={title}
-          classTitle="h3_DD"
-          content={info}
-        />
+        <Dropdown title={title} classTitle="h3_DD" content={info} />
       </div>
       <ListPictures pictures={pictures} />
     </main>
